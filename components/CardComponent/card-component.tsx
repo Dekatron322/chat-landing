8540603f457e9fcee572b2a9b48ad01c6a27c121
Cygnux X-1 <62252129+Dekatron322@users.noreply.github.com>
2025-02-24 03:53:29 +0100

    animated presense

--- components/CardComponent/card-component.tsx
+++ components/CardComponent/card-component.tsx
@@ -1,4 +1,4 @@
-import { motion, AnimatePresence } from "framer-motion"
+import { AnimatePresence, motion } from "framer-motion"
 import React, { useState } from "react"
 
 const CardComponent = () => {
