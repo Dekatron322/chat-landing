a0b3cbba6d7d6cf5385c7f9024bccd68e2e90d91
Cygnux X-1 <62252129+Dekatron322@users.noreply.github.com>
2025-02-25 10:07:47 +0100

    final fix

--- app/privacy-policy/page.tsx
+++ app/privacy-policy/page.tsx
@@ -113,7 +113,7 @@ export default function Dashboard() {
               <h4 className="text-2xl">Lorem Ipsum is simply dummy text of the printing </h4>
               <p>
                 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
-                industr&#39;'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type
+                industr&#39;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type
                 and scrambled it to make a type specimen book. It has survived not only five centuries, but also the
                 leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with
                 the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop
