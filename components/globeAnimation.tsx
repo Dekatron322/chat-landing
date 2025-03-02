dfc7bfb0fcf6ac33b2c67f1d7a40cfa1784b76ed
Cygnux X-1 <62252129+Dekatron322@users.noreply.github.com>
2025-03-02 10:45:38 +0100

    new

--- components/globeAnimation.tsx
+++ components/globeAnimation.tsx
@@ -1,32 +1,7 @@
-import React, { useEffect, useRef } from "react"
-import lottie, { AnimationItem } from "lottie-web"
+import React from "react"
 
-const GlobeAnimation = () => {
-  const container = useRef<HTMLDivElement>(null)
-  const animationInstance = useRef<AnimationItem | null>(null) // Properly typed ref
-
-  useEffect(() => {
-    if (container.current && !animationInstance.current) {
-      // Load the animation only if it hasn't been loaded already
-      animationInstance.current = lottie.loadAnimation({
-        container: container.current, // the DOM element that will contain the animation
-        renderer: "svg", // the rendering method
-        loop: true, // whether the animation should loop
-        autoplay: true, // whether the animation should play as soon as it is ready
-        animationData: require("/public/Globe (1).json"), // the path to your JSON file
-      })
-    }
-
-    // Cleanup function to destroy the animation when the component unmounts
-    return () => {
-      if (animationInstance.current) {
-        animationInstance.current.destroy() // Destroy the animation instance
-        animationInstance.current = null // Reset the reference
-      }
-    }
-  }, []) // Empty dependency array ensures this runs only once
-
-  return <div ref={container} className="w-[722px]"></div>
+const globeAnimation = () => {
+  return <div></div>
 }
 
-export default GlobeAnimation
+export default globeAnimation
