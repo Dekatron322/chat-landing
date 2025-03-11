dbb504b1947c9c9e73c9580be5eacc30f2bf1517
Cygnux X-1 <62252129+Dekatron322@users.noreply.github.com>
2025-03-11 13:46:31 +0100

    mobile view

--- components/CardComponent/changing-lives.tsx
+++ components/CardComponent/changing-lives.tsx
@@ -29,7 +29,7 @@ const ChangingLives = () => {
       role: "Donor",
       image: "/gradient/Frame 1618874027.png",
       text: "Knowing exactly where my contributions go gives me peace of mind. CHATS’ detailed reports and updates are game-changers for donor confidence.",
-      position: { left: "5%", top: "15%" },
+      position: { left: "5%", top: "25%" },
       circleColor: "#FF6B6B", // Unique color for each circle
       ellipseImage: "/gradient/Ellipse 1834.png", // Unique ellipse image for this section
     },
@@ -38,7 +38,7 @@ const ChangingLives = () => {
       role: "Beneficiary",
       image: "/gradient/Frame 1618874030.png",
       text: "Knowing exactly where my contributions go gives me peace of mind. CHATS’ detailed reports and updates are game-changers for donor confidence.",
-      position: { left: "50%", top: "50%" },
+      position: { left: "40%", top: "50%" },
       circleColor: "#4ECDC4", // Unique color for each circle
       ellipseImage: "/gradient/Component 162.png", // Unique ellipse image for this section
     },
@@ -66,21 +66,23 @@ const ChangingLives = () => {
   }
 
   return (
-    <div className="flex items-center py-16 max-sm:flex-col">
-      <div className="paddings flex flex-col items-start gap-6">
+    <div className="flex items-center py-16 max-sm:flex-col-reverse">
+      <div className="paddings flex flex-col items-start gap-6 max-sm:items-center max-sm:justify-center">
         <div className="card flex items-center gap-2 rounded-full px-2 py-2 xl:px-[20px]">
           <img src="/chats transparent 1.png" alt="" className="w-7" />
           <p className="text-sm">Voices of Impact:</p>
         </div>
         <motion.h2
-          className="min-w-[328px] text-4xl font-medium max-sm:text-2xl"
+          className="text-4xl font-medium max-sm:text-2xl xl:min-w-[328px]"
           initial={{ opacity: 0, y: -20 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ ease: "easeOut", duration: 2 }}
         >
           How CHATS is Changing Lives
         </motion.h2>
-        <p className="paragraph min-w-[328px]">These voices reveal the real difference our platform makes every day</p>
+        <p className="paragraph max-sm:text-center xl:min-w-[328px]">
+          These voices reveal the real difference our platform makes every day
+        </p>
         <div className="flex gap-3">
           <BsArrowLeft className="text-[#CDCDCD]" onClick={handlePrevious} />
           <BsArrowRight className="text-[#05F29A]" onClick={handleNext} />
@@ -90,7 +92,7 @@ const ChangingLives = () => {
         {isDarkMode ? (
           <img src="/gradient/image 2 (1).png" alt="" className="h-full w-full object-fill" />
         ) : (
-          <img src="/gradient/image 2.png" alt="" className="h-full w-full object-fill" />
+          <img src="/gradient/image 2.webp" alt="" className="h-full w-full object-fill" />
         )}
         {sections.map((section, index) => (
           <motion.div
