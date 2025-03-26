dcfe5e9a7bb01cc506df7543dd7522920d323c21
Cygnux X-1 <62252129+Dekatron322@users.noreply.github.com>
2025-03-26 14:41:08 +0100

    done

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,6 +12,23 @@ export const metadata: Metadata = {
   title: "Chats",
   description:
     "CHATS transforms the future of humanitarian efforts with verifiable solutions for cash and voucher aid. Our platform enhances donor confidence and enables NGOs to deliver impactful, transparent campaign",
+  icons: {
+    icon: [
+      // Basic favicon
+      { url: "/chats transparent 1.png" },
+      // New favicon for modern browsers
+      { url: "/chats transparent 1.png", sizes: "16x16", type: "image/png" },
+      { url: "/chats transparent 1.png", sizes: "32x32", type: "image/png" },
+    ],
+    apple: [
+      // Apple touch icon
+      { url: "/apple-touch-icon.png" },
+    ],
+    other: [
+      // For other platforms
+      { rel: "mask-icon", url: "/safari-pinned-tab.svg", color: "#5bbad5" },
+    ],
+  },
   twitter: {
     card: "summary_large_image",
   },
